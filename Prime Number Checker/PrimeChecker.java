155601389880aec9ac426539010035695859ac38
Bhuwan <bhuwantiwari2002@gmail.com>
2022-01-08 15:21:21 -0600

    Basic Prime Checker Program

--- Prime Number Checker/PrimeChecker.java
+++ Prime Number Checker/PrimeChecker.java
@@ -9,7 +9,7 @@ public class PrimeChecker {
         int userNumber = input.nextInt(); // Taking the user input
 
         // The code below will check if the number is prime or not
-        boolean prime = false;
+        boolean primeChecker = false;
         for (int c = 2; c*c <= userNumber / 2; c++) { // This loop will run until c is less or equal to the user number
         /* The reason for square rooting (c*c and dividing) is to optimize performance. 
          The square root will avoid the same number being multipled. Let say the computer does 
@@ -18,11 +18,11 @@ public class PrimeChecker {
          */
             // This condition checks does two things, it calculates the remainder and checks if it is equal to 0
             if (userNumber % c == 0) { 
-                prime = true; // If the condition is true, the boolean prime will be changed to true
+                primeChecker = true; // If the condition is true, the boolean prime will be changed to true
                 break;
             }
         }
-        if (!prime) {
+        if (!primeChecker) {
             System.out.println(userNumber + " is prime");
         } else {
             System.out.println(userNumber + " is not prime");
